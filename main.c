17ee8360fd6ddd0f12c340dbde784216c94200b9
Sabi Viji <sabipalathinkal10@gmail.com>
2021-03-02 19:54:50 +0530

    starting menu done

--- main.c
+++ main.c
@@ -4,6 +4,27 @@ TIC TAK TOE
 
 #include<stdio.h>
 
+int menu();
+
 int main(){
+    int m;
+    m=menu();
     return 0;
+}
+
+int menu(){
+    int ch1;
+    printf("\tTIC TAC TOE\n");
+    printf("\t===========\n");
+    printf("\n1. Player vs Player\n2. Player vs Computer\n3. Quit\n");
+    while(1){
+        printf("\nChoose an option: "); 
+        scanf("%d",&ch1);
+        if(ch1>0&&ch1<4){
+            return ch1;
+        }
+        else{
+            printf("Invalid option...try agian");
+        }
+    }
 }
